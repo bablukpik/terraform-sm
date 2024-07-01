804208e6b4859424dd4d5fb03823b551b3480a95
Bablu Ahmed <bablukpik@gmail.com>
2024-07-02 05:48:44 +0600

    master: aws s3 bucket creation config added

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "my_file_bucket_resource" {
+  bucket = "my-file-bucket-${var.env}"
+}
